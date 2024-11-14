459c811122c6e494bf1af8561cb9e73249e04396
Jongkuk Lim <lim.jeikei@gmail.com>
2024-11-14 17:57:47 +0900

    Fix type error

--- recommender_systems/trainer/train_pytorch_matrix_factorization.py
+++ recommender_systems/trainer/train_pytorch_matrix_factorization.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model = TorchMatrixFactorizationModel(  # pylint: disable=invalid-name
-        dataset.user_ids.shape[0], dataset.item_ids.shape[0], K, global_item_bias
+        dataset.user_ids.shape[0], dataset.item_ids.shape[0], K, float(global_item_bias)
     ).to(device)
 
     if os.path.exists(MODEL_PATH):
